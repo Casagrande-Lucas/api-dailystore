cbe1f00bdf7f31b6aa1638cb234e0e06a0a9143d
Lucas Casagrande <dev.casagrande@gmail.com>
2023-12-29 01:28:35 -0300

    fix: using json decode.

--- app/Console/Commands/ImportListProducts.php
+++ app/Console/Commands/ImportListProducts.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Product;
 use App\Services\ServiceCsv;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 class ImportListProducts extends Command
@@ -38,7 +39,7 @@ class ImportListProducts extends Command
         $headers = explode(';', array_shift($data)[0]);
 
         foreach ($data as $row) {
-            $productData = array_combine($headers, explode(';', $row));
+            $productData = array_combine($headers, explode(';', $row[0]));
 
 
             $decodeUnicode = function ($value) {
